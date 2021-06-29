84b9e382f404122fc904c13cc531c7cc2ac0e229
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-06-29 16:28:32 +0200

    Testing husky :)

--- src/views/App.js
+++ src/views/App.js
@@ -1,15 +1,12 @@
 import logo from 'assets/logo.svg';
 import './App.css';
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Jakub Michał Fedoszczak
-        </p>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>Jakub Michał Fedoszczak</p>
         <a
           className="App-link"
           href="https://reactjs.org"
