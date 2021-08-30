fa039c4204f485fcc6b549c8e365c59df5584cee
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-08-30 18:51:18 +0200

    Add handling error for Feed view

--- src/views/Feed/Feed.js
+++ src/views/Feed/Feed.js
@@ -7,23 +7,38 @@ const Feed = () => {
   const { id } = useParams();
   const [article, setArticle] = useState({});
   const [isLoading, setLoadingState] = useState(true);
+  const [hasError, setError] = useState(null);
 
   useEffect(() => {
     setLoadingState(true);
     (async () => {
-      const {
-        data: {
-          data: { allArticles },
-        },
-      } = await axios.post(
-        'https://graphql.datocms.com/',
-        { query: `{allArticles(filter: {id:{eq: "${id}"}}) {id,title,category,feed,image{url}}}` },
-        { headers: { Authorization: `Bearer ${process.env.REACT_APP_DATOCMS_TOKEN}` } }
-      );
-      setArticle(allArticles[0]);
-      setLoadingState(false);
+      try {
+        const {
+          data: {
+            data: { allArticles },
+          },
+        } = await axios.post(
+          'https://graphql.datocms.com/',
+          { query: `{allArticles(filter: {id:{eq: "${id}"}}) {id,title,category,feed,image{url}}}` },
+          { headers: { Authorization: `Bearer ${process.env.REACT_APP_DATOCMS_TOKEN}` } }
+        );
+        setArticle(allArticles[0]);
+        setLoadingState(false);
+      } catch (e) {
+        setError(e.message);
+      }
     })();
   }, [id]);
+
+  if (hasError || article === undefined) {
+    return (
+      <Wrapper>
+        <StyledTitle>Sorry, something went wrong!</StyledTitle>
+        <StyledSubTitle>We have small problem. Check your url!</StyledSubTitle>
+        <p>Looks like you are trying to broke something... but not with us!</p>
+      </Wrapper>
+    );
+  }
 
   return (
     <Wrapper>
